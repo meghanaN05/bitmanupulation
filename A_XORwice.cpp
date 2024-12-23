a3e2d039e76194984387d4b23ae81bbd0aeb703c
Meghana N <meghananoochila@gmail.com>
2024-12-23 23:30:36 +0530

    Add files via upload

---
+++ A_XORwice.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long
+#define endl '\n'
+
+void solve() {
+    int a,b;
+    cin>>a>>b;
+    cout<<(a^b)<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int k;
+    cin >> k;
+
+    while (k--) {
+        solve();
+    }
+
+    return 0;
+
+}
